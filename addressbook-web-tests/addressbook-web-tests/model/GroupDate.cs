1e44180812c0154d3a810482445f341b7e3b2ab8
Юлия Кортелёва <q_lja@mail.ru>
2018-08-13 11:19:58 -0400

    Test architecture

--- addressbook-web-tests/addressbook-web-tests/model/GroupDate.cs
+++ addressbook-web-tests/addressbook-web-tests/model/GroupDate.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebAddressbookTests
 {
-    class GroupDate
+    public class GroupDate
     {
         private string name;
         private string header = "";
---
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void GroupCreationTest()
+        {
+            GroupDate group = new GroupDate("aaa");
+            group.Header = "ddd";
+            group.Footer = "fff";
+
+            app.Groups.Create(group);
+        }
+
+        [Test]
+        public void EmptyGroupCreationTest()
+        {
+            GroupDate group = new GroupDate("");
+            group.Header = "";
+            group.Footer = "";
+
+            app.Groups.Create(group);
+        }
+    }
+}
