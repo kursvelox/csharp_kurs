a8bb7020a941984d298acc420794f818891e940b
Юлия Кортелёва <q_lja@mail.ru>
2018-08-14 04:34:44 -0400

    GroupModificationTests

--- addressbook-web-tests/addressbook-web-tests/appmanager/GroupHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/GroupHelper.cs
@@ -20,8 +20,19 @@ namespace WebAddressbookTests
         public GroupHelper Remove(int v)
         {
             manager.Navigator.GoToGroupsPage();
-            SelectGroup(1);
+            SelectGroup(v);
             RemoveGroup();
+            ReturnToGroupsPage();
+            return this;
+        }
+
+        public GroupHelper Modify(int v, GroupDate newData)
+        {
+            manager.Navigator.GoToGroupsPage();
+            SelectGroup(v);
+            InitGroupModification();
+            FillGroupForm(newData);
+            SubmitGroupModification();
             ReturnToGroupsPage();
             return this;
         }
@@ -76,6 +87,18 @@ namespace WebAddressbookTests
         public GroupHelper RemoveGroup()
         {
             driver.FindElement(By.Name("delete")).Click();
+            return this;
+        }
+
+        public GroupHelper SubmitGroupModification()
+        {
+            driver.FindElement(By.Name("update")).Click();
+            return this;
+        }
+
+        public GroupHelper InitGroupModification()
+        {
+            driver.FindElement(By.Name("edit")).Click();
             return this;
         }
     }
